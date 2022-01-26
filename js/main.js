d3f7d1a47da6d91ac4fe00eab8dc896975a08202
JuanDiRiv <juandirivero@gmail.com>
2022-01-25 23:00:00 -0500

    pagina v1.5

--- js/main.js
+++ js/main.js
@@ -1,114 +1,104 @@
-$(document).ready(function(){
+$(document).ready(function () {
   /***** Mostrar/Ocultar menu mobil *****/
-  $(".show-close-menu-m").click(function(){
-    var menu=$(".navigation");
-    if(menu.css("opacity")=="0"){ 
+  $(".show-close-menu-m").click(function () {
+    var menu = $(".navigation");
+    if (menu.css("opacity") == "0") {
       menu.addClass("show-navigations");
-      $(this).css({ "color":"#2e4b9a" });
-    }else{ 
-      menu.removeClass("show-navigations"); 
-      $(this).css({ "color":"#fff" });
+      $(this).css({ color: "#2e4b9a" });
+    } else {
+      menu.removeClass("show-navigations");
+      $(this).css({ color: "#fff" });
     }
   });
-	/***** Mostrar/ocultar mega-menu *****/
-  $('.btm-mega-menu').click(function(e){
+  /***** Mostrar/ocultar mega-menu *****/
+  $(".btm-mega-menu").click(function (e) {
     e.preventDefault();
- 		var megamenu=$(".mega-menu");
- 		if(megamenu.css("opacity")=="0"){ 
- 			megamenu.addClass("show-mega-menu"); 
- 			$('.btm-mega-menu').css({ "color":"#2e4b9a" });
- 		}else{ 
- 			megamenu.removeClass("show-mega-menu");
- 			$('.btm-mega-menu').css({ "color":"#fff" });
- 		}
-  });  
+    var megamenu = $(".mega-menu");
+    if (megamenu.css("opacity") == "0") {
+      megamenu.addClass("show-mega-menu");
+      $(".btm-mega-menu").css({ color: "#2e4b9a" });
+    } else {
+      megamenu.removeClass("show-mega-menu");
+      $(".btm-mega-menu").css({ color: "#fff" });
+    }
+  });
   /***** Abrir link en una nueva ventana *****/
-  $(".open-link-newTab").click(function(e){
- 		e.preventDefault();
- 		var HrefLink=$(this).attr("href");
- 		window.open(HrefLink,'_blank');
+  $(".open-link-newTab").click(function (e) {
+    e.preventDefault();
+    var HrefLink = $(this).attr("href");
+    window.open(HrefLink, "_blank");
   });
   /***** Moverse el la pagina institucion con Scroll *****/
-	$('.scroll-navigation-ins ul li').click(function(){
-		var seccion=$(this).attr('data-href');
-	$('body,html').animate({
-	  scrollTop:$(seccion).offset().top-70
-	},1000);
-	return false; 
-	});
+  $(".scroll-navigation-ins ul li").click(function () {
+    var seccion = $(this).attr("data-href");
+    $("body,html").animate(
+      {
+        scrollTop: $(seccion).offset().top - 70,
+      },
+      1000
+    );
+    return false;
+  });
   /***** boton ir arriba *****/
-  $('.btn-up').click(function(){
-    $('body,html').animate({scrollTop:'0px'}, 300);
+  $(".btn-up").click(function () {
+    $("body,html").animate({ scrollTop: "0px" }, 300);
   });
   /*****Mostrar y ocultar boton ir arriba *****/
-  $(window).scroll(function(){
-    if($(this).scrollTop() >= 500){
-      $('.btn-up').fadeIn();
-    }else{
-      $('.btn-up').fadeOut();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 500) {
+      $(".btn-up").fadeIn();
+    } else {
+      $(".btn-up").fadeOut();
     }
   });
-
-
-
 });
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
+  let cerrar = document.querySelectorAll(".close")[0];
+  let abrir = document.querySelectorAll(".cta")[0];
+  let modal = document.querySelectorAll(".modal")[0];
+  let modalC = document.querySelectorAll(".modal-container")[0];
+  const formulario = this.document.forms[0];
+  let inputUsuario = document.querySelector("#usuario");
+  let inputContrasenia = document.querySelector("#password");
 
-    let cerrar = document.querySelectorAll('.close')[0];
-    let abrir = document.querySelectorAll('.cta')[0];
-    let modal = document.querySelectorAll('.modal')[0];
-    let modalC = document.querySelectorAll('.modal-container')[0];
-    const formulario =  this.document.forms[0];
-    let inputUsuario = document.querySelector("#usuario");
-    let inputContrasenia = document.querySelector("#password");
-    
-    abrir.addEventListener('click', function(e) {
-        e.preventDefault();
-        modalC.style.opacity ="1"
-        modalC.style.visibility = "visible"
-        modal.classList.toggle("modal-close")
-    });
-    
-    cerrar.addEventListener('click', function(e) {
-        e.preventDefault();
-            modal.classList.toggle("modal-close")
-            modalC.style.opacity ="0"
-            modalC.style.visibility = "hidden"
-    
-    
-       
-    })
-    
-    formulario.addEventListener('submit', function(e){
-        e.preventDefault();
-        let usuario = inputUsuario.value;
-        let contrasenia = inputContrasenia.value;
-    
-        if(usuario == "admin" && contrasenia == "admin"){
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'Your work has been saved',
-                showConfirmButton: true,
-                
-              })
-              setTimeout(function(){
+  abrir.addEventListener("click", function (e) {
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+  });
 
-                  location.href="../descarga.html";
-              },2000)
-             
-            
-        
-            }else{
-                Swal.fire({
-                    position: 'center',
-                    icon: 'error',
-                    title: 'Usuario o contraseña incorrecta',
-                    showConfirmButton: true,
-                    
-                })
-    
-            }
-            formulario.reset();
-    })
-    });
+  cerrar.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.toggle("modal-close");
+    modalC.style.opacity = "0";
+    modalC.style.visibility = "hidden";
+  });
+
+  formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let usuario = inputUsuario.value;
+    let contrasenia = inputContrasenia.value;
+
+    if (usuario == "admin" && contrasenia == "admin") {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Usuario y contraseña correctos",
+        showConfirmButton: true,
+      });
+
+      setTimeout(function () {
+        location.pathname = "../descarga.html";
+      }, 2000);
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Usuario o contraseña incorrecta",
+        showConfirmButton: true,
+      });
+    }
+    formulario.reset();
+  });
+});
